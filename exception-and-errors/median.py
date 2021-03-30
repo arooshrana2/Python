dcbe4d8d10b08cb1145d044e6df27ce6a2f9f96e
Aroosh Rana <arooshrana2@gmail.com>
2021-03-30 23:58:29 +0530

    Update median.py

--- exception-and-errors/median.py
+++ exception-and-errors/median.py
@@ -1,8 +1,18 @@
+
 def get_median(list_of_items):
+    """
+    Usage: Finds the median from a list of numbers provided
+    
+    Parameters:
+        list_of_items: A list of items(numbers asof now)
+    Return:
+        median: A float number indicating the median     
+    """
     _list = sorted(list_of_items)
 
     if len(_list) == 0:
-        raise ValueError("get_median() arg is empty")
+        raise ValueError("get_median() arg is empty")  # Exception needs to be raised if len is 0
+                                                       # Returning exception will just print the message here
 
     mid_ele = int(len(_list)//2)
 
@@ -12,6 +22,7 @@ def get_median(list_of_items):
 
 
 def main():
+    # Creating random 5 test cases to see if the program works
     x = 5
     twoD_array = [
         [4,6,1,7,9,10],
@@ -25,7 +36,7 @@ def main():
         try:
             median = get_median(twoD_array[x])
             print("Here => ", median)
-        except Exception as e:
+        except Exception as e:  # Catching the raised exception
             print("Payload", e.args)
             print("Payload {}".format(str(e)))
             continue
